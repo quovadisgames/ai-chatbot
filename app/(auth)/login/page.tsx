0cc90caa37996911b973cd5818c7cb8c19ba34bc
Toni <quovadisgames@gmail.com>
2025-03-03 15:05:32 -0800

    Updated

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -40,7 +40,7 @@ export default function Page() {
   };
 
   return (
-    <div className="flex h-dvh w-screen items-start pt-12 md:pt-0 md:items-center justify-center bg-background">
+    <div className="flex h-dvh w-screen items-start pt-12 md:pt-0 md:items-center justify-center bg-blue-200 dark:bg-blue-950">
       <div className="w-full max-w-md overflow-hidden rounded-2xl flex flex-col gap-12">
         <div className="flex flex-col items-center justify-center gap-2 px-4 text-center sm:px-16">
           <h3 className="text-xl font-semibold dark:text-zinc-50">Sign In</h3>

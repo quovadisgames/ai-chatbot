01fa1768f594e111adfac954607320dad77d3b0f
Toni <quovadisgames@gmail.com>
2025-03-07 21:14:43 -0800

    Update actions.ts

--- app/(auth)/actions.ts
+++ app/(auth)/actions.ts
@@ -61,7 +61,7 @@ export const register = async (
       password: formData.get('password'),
     });
 
-    const [user] = await getUser(validatedData.email);
+    const [user] = await getUser({ userId: validatedData.email });
 
     if (user) {
       return { status: 'user_exists' } as RegisterActionState;

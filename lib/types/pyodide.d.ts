611b42302e0c7d3072d6a31207a0eaf3ce09f70d
Toni <quovadisgames@gmail.com>
2025-03-17 07:58:00 -0700

    12

--- lib/types/pyodide.d.ts
+++ lib/types/pyodide.d.ts
@@ -1,9 +1,15 @@
+interface PyodideInterface {
+  loadPackagesFromImports(code: string, options?: { messageCallback?: (message: string) => void }): Promise<void>;
+  runPythonAsync(code: string): Promise<any>;
+  setStdout(options: { batched: (output: string) => void }): void;
+}
+
 declare global {
   interface Window {
-    loadPyodide: (options?: { indexURL?: string }) => Promise<any>;
+    loadPyodide: (options?: { indexURL?: string }) => Promise<PyodideInterface>;
   }
   
-  var loadPyodide: (options?: { indexURL?: string }) => Promise<any>;
+  var loadPyodide: (options?: { indexURL?: string }) => Promise<PyodideInterface>;
 }
 
 export {}; 
